1de2ff6f895e10dd7f803ef4066f3b6b0d2c2596
DeepxHyeon <deepxhyeon@gmail.com>
2020-05-29 21:13:30 +0900

    Day 5 : 문제 001 - 문제 007

---
+++ test/001.py
@@ -0,0 +1,7 @@
+# 문제 001 : 리스트 삭제
+
+nums = [100, 200, 300, 400, 500]
+
+nums.remove(400)
+nums.remove(500)
+print(nums)
---
+++ test/005.py
@@ -0,0 +1,10 @@
+# 문제 005 : for문 계산
+
+a = 10
+b = 2
+for i in range(1, 5, 2):
+    a += i
+
+print(a+b)
+
+# 16
